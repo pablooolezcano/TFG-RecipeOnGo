c6cd4b4d2608087bfe4166e6416c38b70cbe99c5
Pablo <pablolezsan@gmail.com>
2023-07-30 16:01:10 +0100

    Read and Write shopping list correctly implemented from Firestore
    Read favourites from firestore

--- RecipeOnGo/src/app/favourites-page/favourites-page.component.ts
+++ RecipeOnGo/src/app/favourites-page/favourites-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { getFirestore, doc,getDoc, setDoc, updateDoc } from "firebase/firestore";
+import { environment } from 'src/environments/environment';
+import { initializeApp } from "firebase/app";
 @Component({
   selector: 'app-favourites-page',
   templateUrl: './favourites-page.component.html',
@@ -7,8 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavouritesPageComponent  implements OnInit {
 
+  user_uid: string | null = localStorage.getItem('user_login_uid');
+  favouritesIdsList: any;
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    const firebaseConfig = environment.firebaseConfig;
+    const app = initializeApp(firebaseConfig);
+    
+    this.getFireDatabaseDoc();
+  }
 
+  async getFireDatabaseDoc(){
+    const db = getFirestore();
+    const docRef = doc(db, "favourites", "" + this.user_uid);
+
+    try{
+      const docSnap = await getDoc(docRef);
+      if(docSnap.exists()){
+        this.favouritesIdsList = docSnap.data()['ids'];
+        console.log(this.favouritesIdsList)
+      }
+    } catch(error) {
+      console.log(error)
+    }
+  }
 }
--- RecipeOnGo/src/app/shopping-cart-page/shopping-cart-page.component.ts
+++ RecipeOnGo/src/app/shopping-cart-page/shopping-cart-page.component.ts
@@ -1,13 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set, onValue } from "firebase/database";
 import { environment } from 'src/environments/environment';
-import { inject } from '@angular/core';
-//import { Firestore, collectionData, collection, getFirestore, addDoc } from '@angular/fire/firestore';
-import { Firestore, collection, addDoc } from 'firebase/firestore';
-import { getFirestore, doc, getDocs, setDoc, deleteField, updateDoc } from "firebase/firestore";
-import { } from '@angular/fire/functions';
-//import { setDoc } from '@angular/fire/firestore';
+import { getFirestore, doc,getDoc, setDoc, updateDoc } from "firebase/firestore";
 @Component({
   selector: 'app-shopping-cart-page',
   templateUrl: './shopping-cart-page.component.html',
@@ -15,10 +9,10 @@ import { } from '@angular/fire/functions';
 })
 export class ShoppingCartPageComponent  implements OnInit {
 
-  shoppingList: any;
+  shoppingList: any = [""];
   aux: any;
   inputText: string = "";
-  //firestore: Firestore = inject(Firestore);
+  user_uid: string | null = localStorage.getItem('user_login_uid');
 
   constructor() { }
 
@@ -26,63 +20,56 @@ export class ShoppingCartPageComponent  implements OnInit {
     const firebaseConfig = environment.firebaseConfig;
     const app = initializeApp(firebaseConfig);
     
-    //const dbRef = collection(db, "shopping-lists");
-    //this.shoppingList = collectionData(itemCollection);
-    console.log("Hola");
-    this.getFireDatabase();
-    //debugger
+    this.getFireDatabaseDoc();
 
-
-    // Initialize Realtime Database and get a reference to the service
-    // const db = getDatabase();
-    // const shoppingListResponse = ref(db, 'shopping-list/' + 1);
-
-    // onValue(shoppingListResponse, (snapshot) => {
-    //   console.log(snapshot.val());
-    //   this.aux = snapshot.val();
-    //   const data = Object.values(snapshot.val());
-    //   console.log(data);
-    //   this.shoppingList = data;
-    // });
   }
   
-  async getFireDatabase(){
+  async getFireDatabaseDoc(){
     const db = getFirestore();
-    const colRef = collection(db, "shopping-lists");
-    const docsSnap = await getDocs(colRef);
-    docsSnap.forEach(doc => {
-      this.shoppingList = doc.data()['list'];
-      //console.log(doc.data());
-    })
-    console.log(this.shoppingList);
+    const docRef = doc(db, "shopping-lists", "" + this.user_uid);
+
+    try{
+      const docSnap = await getDoc(docRef);
+      if(docSnap.exists()){
+        this.shoppingList = docSnap.data()['list'];
+        console.log(this.shoppingList)
+      }
+    } catch(error) {
+      console.log(error)
+    }
   }
   add_to_database(){
-    // Initialize Firebase
+
+    const firebaseConfig = environment.firebaseConfig;
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+
+    let list;
     let inputText = document.getElementById("input_text") as HTMLInputElement;
     this.inputText = inputText.value;
-    const firebaseConfig = environment.firebaseConfig;
-    const app = initializeApp(firebaseConfig);
-    const db = getFirestore(app);
-    //const colRef = collection(db, "shopping-lists");
-    console.log(this.shoppingList);
-    this.shoppingList.push(this.inputText);
-    //esto de aqui debe ser con el user_uid
-    const docRef = doc(db, "shopping-lists", "22YyBDzCYfDiH7zUPXWS");
+    if(this.inputText != ""){
+      list = this.shoppingList;
+      list.push(this.inputText);
+    }
+    if(this.user_uid != null){
+      const docRef = doc(db, "shopping-lists", "" + this.user_uid);
     let data = {
-      "list": this.shoppingList
+      "list": list
     };
     setDoc(docRef, data);
+    }
 
-  console.log("Done ?");
   }
   deleteList(){
-    console.log("Deleting");
+    
     const firebaseConfig = environment.firebaseConfig;
     const app = initializeApp(firebaseConfig);
     const db = getFirestore(app);
-    const docRef = doc(db, "shopping-lists", "22YyBDzCYfDiH7zUPXWS");
+
+    const docRef = doc(db, "shopping-lists", "" + this.user_uid);
+    this.shoppingList = [""];
     const data = {
-      "list": deleteField()
+      "list": this.shoppingList
     }
     updateDoc(docRef, data)
   }
@@ -92,4 +79,12 @@ export class ShoppingCartPageComponent  implements OnInit {
 
   }
 
+  //Get All Documents from a FireStoreCollection:
+  // const colRef = collection(db, "shopping-lists");
+    // const docsSnap = await getDocs(colRef);
+    // docsSnap.forEach(doc => {
+
+    //   this.shoppingList = doc.data()['list'];
+    //   //console.log(doc.data());
+    // })
 }
