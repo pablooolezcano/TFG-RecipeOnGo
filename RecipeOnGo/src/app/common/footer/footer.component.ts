bb7f5d11eda600ca0412c731eb70fb07941405d9
Pablo <pablolezsan@gmail.com>
2023-10-26 17:59:05 +0100

    Footer now desappears when native keyboard is open
    And footer toolbar is hide too when app is running on web platform device

--- RecipeOnGo/src/app/common/footer/footer.component.ts
+++ RecipeOnGo/src/app/common/footer/footer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router} from '@angular/router';
+import { Device } from '@capacitor/device';
+import { Keyboard } from '@capacitor/keyboard';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -8,6 +10,9 @@ import { Router} from '@angular/router';
 export class FooterComponent  implements OnInit {
 
   isLogged: boolean = false;
+  platform: string = "";
+  keyboardActive: boolean = false;
+  showFooter: boolean = true;
   constructor(private router: Router) { }
 
   ionViewWillEnter(){
@@ -19,8 +24,37 @@ export class FooterComponent  implements OnInit {
 
   ngOnInit() {
     this.isLogged = false;
-    if(localStorage.getItem('user_login_uid')){
+    if (localStorage.getItem('user_login_uid')) {
       this.isLogged = true;
+    }
+    this.getDeviceInfo();
+
+    if(this.platform !== 'web'){
+      Keyboard.addListener('keyboardDidShow', () => {
+        this.keyboardActive = true;
+        this.changeShowFooter();
+      });
+      Keyboard.addListener('keyboardDidHide', () => {
+        this.keyboardActive = false;
+        this.changeShowFooter();
+      });
+    }
+  }
+  async getDeviceInfo() {
+    const info = await Device.getInfo();
+    this.platform = info.platform;
+    console.log(this.platform);
+    this.changeShowFooter();
+  }
+  changeShowFooter() {
+    if (this.platform == 'web') {
+      this.showFooter = false;
+    } else {
+      if (this.keyboardActive == true) {
+        this.showFooter = false;
+      } else {
+        this.showFooter = true;
+      }
     }
   }
 
